3bbf6345d999526215eb11be914d47732a729ccd
Selin <76973776+monniemoon@users.noreply.github.com>
2021-10-14 15:11:25 +0300

    Update Form1.cs

--- Windows-Forms/LAB1/LAB1-2code/Form1.cs
+++ Windows-Forms/LAB1/LAB1-2code/Form1.cs
@@ -1,1 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
+namespace Upr1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_plus_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            try
+            {
+                a = Double.Parse(textBox_a.Text);
+                b = Double.Parse(textBox_b.Text);
+                textBox_result.Text = (a + b).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Oops!");
+                textBox_a.Clear();
+                textBox_b.Clear();
+            }
+        }
+
+        private void button_minus_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            try
+            {
+                a = Double.Parse(textBox_a.Text);
+                b = Double.Parse(textBox_b.Text);
+                textBox_result.Text = (a - b).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Oops!");
+                textBox_a.Clear();
+                textBox_b.Clear();
+            }
+        }
+
+        private void button_multiply_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            try
+            {
+                a = Double.Parse(textBox_a.Text);
+                b = Double.Parse(textBox_b.Text);
+                textBox_result.Text = (a * b).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Oops!");
+                textBox_a.Clear();
+                textBox_b.Clear();
+            }
+        }
+
+        private void button_divide_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            try
+            {
+                a = Double.Parse(textBox_a.Text);
+                b = Double.Parse(textBox_b.Text);
+                textBox_result.Text = (a / b).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Oops!");
+                textBox_a.Clear();
+                textBox_b.Clear();
+            }
+        }
+    }
+}
