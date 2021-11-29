18106772dbb5cf0faade0b6a221ed0b00f7c471f
Celine K <76973776+moonniemoon@users.noreply.github.com>
2021-11-29 16:18:43 +0200

    Update lab7.sql

--- SQL/LAB7/lab7.sql
+++ SQL/LAB7/lab7.sql
@@ -1,71 +1,69 @@
-/*
-select kolona1, kolona2, ..... 
-from име на таблица от която искаме да вземем данните
-join имена-таблица2
-on employees.pos_id = positions.pos_id 
-where задаваме критерии за търсене 
-group by спписък от полета
-order by списък от полета за сортировка (намаляващ - desc и нарастващ - asc)
-*/
+--създаване на таблица
  
  
--- да се изведе цялата информация от таблицата countries
- 
-SELECT * FROM countries; -- * - абсолютно всички колони изкарва
-SELECT country_id AS ID, country_name AS Country FROM countries;
- 
--- конкатенция на колони 
--- да се изведе конкатинирано първо име и фамилия на служителите
-SELECT first_name || ' ' ||  last_name AS fullname FROM employees; -- || ЗНАК ЗА КОНКАТЕНАЦИЯ
- 
--- агрегатни функции - max, min, avg, sum, count
--- да се намери най-високата заплата във фирмата
-SELECT MAX(salary) AS MAX_salary FROM employees;
-SELECT MIN(salary) AS MIN_salary FROM employees;
-SELECT round(avg(salary),3) AS AVG_salary FROM employees;
-SELECT SUM(salary) AS SUM_salary FROM employees;
- 
--- да прресметнем броя на служителите 
- 
-SELECT COUNT(employee_id) FROM employees;
- 
--- да се пресметне броя на служителите за всеки отдел
-SELECT COUNT(department_id) AS empl_numb, department_id
-FROM employees
-GROUP BY department_id;
- 
--- да се изведе информация за страницате, които имат 3-то ид на региона
-SELECT * FROM countries
-WHERE region_id=3;
- 
--- да се изведе информация за служителите - първо име, фамилия, заплата, дата на назначаване и позиция която заема служителя с ST_CLERK job_id
-SELECT first_name, last_name, salary, hire_date, job_id FROM employees
-WHERE job_id = 'ST_CLERK'; -- =  tyrsi cqlostno sywpadenie
- 
--- да се изведе информация за служителите - първо име, фамилия, заплата, дата на назначаване и позиция която 
---заема служителя имащ 'ne' във фамилията си
-SELECT first_name, last_name, salary, hire_date, job_id FROM employees
-WHERE last_name LIKE '%ne%';
-/*
-'%ne'  - търси в края 
-'ne%' - търси в началото
-'%ne%'  - търси навсякъде
-*/
---да изведем изформация за служителите s data назначение за периода 01-01-2005 и 31-12-2006
-SELECT first_name, last_name, salary, hire_date, job_id FROM employees
-WHERE hire_date >= '2005-01-01' AND hire_date <= '2006-12-31'
-ORDER BY hire_date;
- 
-SELECT first_name, last_name, salary, hire_date, job_id FROM employees
-WHERE hire_date BETWEEN '2005-01-01' AND  '2005-12-31'
-ORDER BY hire_date;
- 
--- да се изведе информация за служителите, работещи в отдели с номера 20,40,60
-SELECT first_name, last_name, hire_date, department_id FROM employees
-WHERE department_id = 20 OR department_id = 40 OR department_id = 60
-ORDER BY first_name, hire_date ;
+CREATE TABLE Positions
+(
+pos_id INTEGER NOT NULL,
+pos_name VARCHAR(20),
+pos_salary NUMBER (7,2),
+depatrmen_id INT
+);
  
  
-SELECT first_name, last_name, hire_date, department_id FROM employees
-WHERE department_id IN(30,40,60) AND first_name = 'Alexander' 
-ORDER BY first_name, last_name;
+-- добавяне на P.K.
+ALTER TABLE Positions ADD CONSTRAINT pos_PK PRIMARY KEY (pos_id);
+ 
+-- добавяне на колона 
+ALTER TABLE Positions ADD  depatrmen_id VARCHAR (20);
+ 
+-- промяна на тип на колона
+ALTER TABLE Positions MODIFY depatrmen_id INT;
+ 
+-- изтриване на таблица
+DROP TABLE Positions;
+ 
+CREATE TABLE employees
+(
+empl_id INT NOT NULL,
+empl_name VARCHAR(20),
+empl_lastN VARCHAR(30),
+empl_hire_date DATE,
+pos_id INT,
+PRIMARY KEY(empl_id)
+);
+ 
+-- създаване на връзка между две таблица P.K. => F.K.
+ 
+ALTER TABLE employees  -- име на таблица, в която е F.K.
+ADD CONSTRAINT  employees_pos_FK  -- име на правилото 
+FOREIGN KEY (pos_id) -- име на колона, която е Ф.К за таблицата
+REFERENCES positions(pos_id); -- име на таблица, където е нашия П.К. и съответно самаа колона П.К.
+ 
+--добавяне на информация в таблицитте
+INSERT INTO positions (pos_id,pos_name,pos_salary)
+VALUES(1,'Sales manager', 1300.00);
+ 
+INSERT INTO positions
+VALUES(3,'Cleaner', 800.00);
+ 
+INSERT INTO employees (empl_id, empl_name, empl_lastN, empl_hire_date, pos_id)
+VALUES ( 4, 'Petq', 'Zheleva', '2016-03-02', 2);
+ 
+-- редакттиране на записи
+UPDATE positions -- име на таблица, в която искаме да направим промяна
+SET pos_name = 'Programmer', -- колона, в която искаме да променяме = нова стойност
+    pos_salary = 1500.00
+WHERE pos_id = 2; --задаваме условие, по което да се направи промяната
+ 
+ 
+UPDATE employees 
+SET empl_name = 'Dimityr',
+    empl_lastn = 'Stoyanov',
+    empl_hire_date = '2010-12-02',
+    pos_id = 3
+WHERE empl_id = 1;
+ 
+-- изтриване на записи от таблица
+ 
+DELETE FROM employees -- таблица, от която искаме да изтрием
+WHERE empl_id =2; -- условие, по което задаваме какво да се изтрие, ако се пропусне ова условие се изрива всички данни в таблицата
